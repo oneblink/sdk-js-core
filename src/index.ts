c984ffb384cf3acfa0b52ef588e731a7e59a0c4b
Zac Turner <zac@oneblink.io>
2021-07-07 15:56:36 +1000

    ON-13340 # Added typeCast service

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as formElementsService from './formElementsService'
 import * as conditionalLogicService from './conditionalLogicService'
 import replaceCustomValues from './replaceCustomValues'
+import * as typeCaseService from './typeCastService'
 
-export { formElementsService, conditionalLogicService, replaceCustomValues }
+export {
+  formElementsService,
+  conditionalLogicService,
+  replaceCustomValues,
+  typeCaseService,
+}
