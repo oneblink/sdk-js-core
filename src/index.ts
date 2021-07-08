49604a4dd6eb3d60b38aa3f2906246352eafb790
Zac Turner <zac@oneblink.io>
2021-07-08 11:59:51 +1000

    Fixed typo in export name

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import * as formElementsService from './formElementsService'
 import * as conditionalLogicService from './conditionalLogicService'
 import replaceCustomValues from './replaceCustomValues'
-import * as typeCaseService from './typeCastService'
+import * as typeCastService from './typeCastService'
 
 export {
   formElementsService,
   conditionalLogicService,
   replaceCustomValues,
-  typeCaseService,
+  typeCastService,
 }
