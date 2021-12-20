d740e22f36d28430115eea4a0bed63360cfe1244
Zac Turner <zac@oneblink.io>
2021-12-20 12:20:29 +1100

    ON-17795 # Added new fns for getting ABN number

--- src/abnService.ts
+++ src/abnService.ts
@@ -1,6 +1,8 @@
 import { ABNRecord } from '@oneblink/types/typescript/misc'
 
-export const getBusinessNameFromABNRecord = (abnRecord: ABNRecord): string => {
+export const getBusinessNameFromABNRecord = (
+  abnRecord: ABNRecord,
+): string | undefined => {
   if (abnRecord.mainName?.organisationName) {
     return abnRecord.mainName.organisationName
   }
@@ -19,6 +21,25 @@ export const getBusinessNameFromABNRecord = (abnRecord: ABNRecord): string => {
 
     if (legalName) return legalName.trim()
   }
+}
+export const displayBusinessNameFromABNRecord = (
+  abnRecord: ABNRecord,
+): string => {
+  const businessName = getBusinessNameFromABNRecord(abnRecord)
+  return businessName || 'Unknown Business Name'
+}
 
-  return 'Unknown Business Name'
+export const getABNNumberFromABNRecord = (
+  abnRecord: ABNRecord,
+): string | undefined => {
+  if (!abnRecord.ABN) return
+  if (Array.isArray(abnRecord.ABN)) {
+    const relevantABN = abnRecord.ABN.find((abn) => abn.isCurrentIndicator)
+    return relevantABN?.identifierValue
+  }
+  return abnRecord.ABN.identifierValue
+}
+export const displayABNNumberFromABNRecord = (abnRecord: ABNRecord): string => {
+  const abnNumber = getABNNumberFromABNRecord(abnRecord)
+  return abnNumber || 'Unknown ABN Number'
 }
