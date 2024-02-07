0b76f51cd1a893b9e2a21225b631157d2b8153ba
Ryan Button <ryan@oneblink.io>
2024-02-07 16:07:58 +1100

    ON-37947 # update types arcgis web map

--- src/formElementsService.ts
+++ src/formElementsService.ts
@@ -289,6 +289,7 @@ const infoPageElements: FormTypes.FormElementType[] = [
   'page',
   'infoPage',
   'form',
+  'arcGISWebMap'
 ]
 /**
  * Determine a form is considered an info page. This means the form does not
