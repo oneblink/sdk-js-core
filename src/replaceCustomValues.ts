3bc68b3814c75653d27117576151055e08ae6582
jdawg093 <49189518+jdawg093@users.noreply.github.com>
2024-05-21 15:02:39 +1000

    ON-40445 # added id as backup if formattedAddress doesn't exist

--- src/replaceCustomValues.ts
+++ src/replaceCustomValues.ts
@@ -449,7 +449,7 @@ function formatValue({
       const value = unknownValue as GoogleTypes.GoogleMapsAddress
       return {
         element: element,
-        value: value.formattedAddress,
+        value: value.formattedAddress || value.id,
       }
     }
     case 'civicaStreetName': {
